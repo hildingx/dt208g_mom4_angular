055834f00809b8626e4bbd7a67fae883b3f12d10
Alexander Hilding <hildingx@gmail.com>
2024-04-25 14:20:05 +0200

    Lagt till funktionalitet för att skriva ut hämtad data i tabell, samt implementerat sökfunktion + styles på tabell.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
@@ -14,6 +15,8 @@ import { CommonModule } from '@angular/common';
 export class CoursesComponent {
   //Egenskaper
   courses: Course[] = [];
+  searchedCourses: Course[] = [];
+  searchValue: string = "";
 
   constructor(private courseService: CoursesService) {}
 
@@ -21,6 +24,14 @@ export class CoursesComponent {
   ngOnInit() {
     this.courseService.getCourses().subscribe((courses) => {
       this.courses = courses;
+      this.searchedCourses = courses;
     });
+  }
+
+  applySearch(): void {
+    this.searchedCourses = this.courses.filter((course) =>
+      course.code.toLowerCase().includes(this.searchValue.toLowerCase())||
+      course.coursename.toLowerCase().includes(this.searchValue.toLowerCase())
+    );
   }
 }
